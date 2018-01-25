656d7627228aa938338107489357f2b284dfe571
MushuLeDragon <dragon.mushu@hotmail.com>
2018-01-25 14:00:38 +0100

    add component matches list

---
+++ src/components/MyMatches.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <p>
+          Liste de mes matchs
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'my-matches'
+
+}
+</script>
+
+<style>
+
+</style>
