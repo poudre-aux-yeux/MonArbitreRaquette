98655611566d7e2a00025b95a2b910bf087c5f5a
Guillaume BAECHLER <baechler.guillaume.pro@gmail.com>
2018-06-15 16:14:58 +0200

    cleaning working files

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import GoodByeWorld from '@/components/GoodByeWorld'
-import Products from '@/components/Products'
-import TestPageJS from '@/components/TestPage'
 import MyMatches from '@/components/MyMatches'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/goodbye',
-      name: 'GoodBye',
-      component: GoodByeWorld
-    },
-    {
-      path: '/produits',
-      name: 'Products',
-      component: Products
-    },
-    {
-      path: '/test',
-      name: 'TestPage',
-      component: TestPageJS
-    },
     {
       path: '/my-matches',
       name: 'MyMatches',
